6e905cb30c5359ea88a2794107c3fb0190a7d81d
AD001\z003zw0m <ioan.ciuciu@siemens.com>
2022-06-27 12:34:37 +0300

    Adding Lists and Tasks with dummy data works

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Model } from 'mongoose';
 import { TaskService } from 'src/app/task.service';
 
 @Component({
@@ -9,22 +10,44 @@ import { TaskService } from 'src/app/task.service';
 })
 export class DashboardComponent implements OnInit {
 
-  lists: any;
+  lists: any[] = [];
   tasks: any;
+  
+  selectedList: any;
 
   constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.getAllLists();
   }
 
   createNewList() {
     this.taskService.createList("Testing").subscribe((response: any) => {
-      console.log(response);
+      this.lists.push(response);
+    }); 
+  }
+
+  getAllLists() {
+    this.taskService.getLists().subscribe((response: any) => {
+      this.lists = response;
+      this.selectedList = this.lists[0];
+      this.getAllTasks(this.selectedList._id);
     });
   }
 
-  addTaskToList() {
+  getAllTasks(listId: any) {
+    this.taskService.getTasks(listId).subscribe((response: any) => {
+      this.tasks = response;
+    });
+  }
 
+  createNewTask() {
+    let task = {title: ''};
+    task.title = "Testiiing";
+
+    this.taskService.createTask(this.selectedList._id, task).subscribe((response: any) => {
+      this.tasks.push(response);
+    })
   }
 
 }
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -13,4 +13,18 @@ export class TaskService {
     // We want to send a web request to create a list
     return this.webReqService.post('lists', {title});
   }
+
+  getLists() {
+    return this.webReqService.get('lists');
+  }
+
+  getTasks(listId : any) {
+    let link = 'lists/' + listId + '/tasks';
+    return this.webReqService.get(link);
+  }
+
+  createTask(listId: any, task: Object) {
+    let link = 'lists/' + listId + '/tasks';
+    return this.webReqService.post(link, task);
+  }
 }
