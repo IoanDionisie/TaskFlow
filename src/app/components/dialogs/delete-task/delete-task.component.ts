204d11f146ed62f4629be508544a241fc7af41af
AD001\z003zw0m <ioan.ciuciu@siemens.com>
2022-06-28 14:44:20 +0300

    Deleting tasks works, and also added a prompt for the user

---
+++ src/app/components/dialogs/delete-task/delete-task.component.ts
@@ -0,0 +1,14 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-delete-task',
+  templateUrl: './delete-task.component.html',
+  styleUrls: ['./delete-task.component.scss']
+})
+
+export class DeleteTaskComponent  {
+
+  constructor(public dialogRef: MatDialogRef<DeleteTaskComponent>) {}
+
+}
