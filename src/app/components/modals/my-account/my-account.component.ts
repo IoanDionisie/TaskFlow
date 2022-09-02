ad7e5fe4f5a95c0ec72cca97576e8a6931354a8f
AD001\z003zw0m <ioan.ciuciu@siemens.com>
2022-09-02 18:02:52 +0300

    Added 'My Account' modal, changing password not finished yet

--- src/app/components/modals/my-account/my-account.component.ts
+++ src/app/components/modals/my-account/my-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -6,15 +7,48 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './my-account.component.html',
   styleUrls: ['./my-account.component.scss']
 })
+
 export class MyAccountComponent implements OnInit {
+
+  submitted = false;
+  working = false;
+  complete = false;
+  strongPassword = false;
+
+  form: any = {
+    confirmPassword: null,
+    password: new FormControl(null, [
+      Validators.minLength(8),
+      Validators.required,
+    ])
+  };
 
   constructor(private modal: NgbActiveModal) { }
 
   ngOnInit(): void {
+    this.form.confirmPassword = "";
   }
 
   closeModal() {
     this.modal.close();
+  }
+
+  
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.working = true;
+
+    const { password } = this.form;
+  
+  }
+
+  onPasswordStrengthChanged(event: boolean) {
+    console.log(this.form.password);
+    this.strongPassword = event;
   }
 
 }
